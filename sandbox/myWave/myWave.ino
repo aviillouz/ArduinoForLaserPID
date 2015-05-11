3876fd221e4f83225e8655827970781365d89f60
aviillouz <avi.illouz@gmail.com>
2015-05-11 15:46:02 +0300

    embed PID into myWave - without control yet
    to be continued in line 30

--- sandbox/myWave/myWave.ino
+++ sandbox/myWave/myWave.ino
@@ -1,29 +1,32 @@
 // myWave.ino
 // #include "Waveforms.h"
+#include <PID_v1.h>
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output;
+
+//Specify the links and initial tuning parameters
+PID myPID(&Input, &Output, &Setpoint,2,5,0, DIRECT);
 
 int value = 0;
 int DACBits = 12;
 int input = 0;
-// unsigned long reg = REG_ADC_MR;
-// REG_ADC_MR = (REG_ADC_MR & 0xFFF0FFFF) | 0x00020000;
 
 void setup() {
-  // Serial.begin(115200);
-	analogWriteResolution(DACBits);  // set the analog output resolution to DACBits bit (4096 levels)
+  Serial.begin(115200);
+	analogWriteResolution(DACBits);  
   analogReadResolution(DACBits);
+  Input = analogRead(A0);
+  Setpoint = 1000;
+
+  //turn the PID on
+  myPID.SetOutputLimits(0,4000);
+  myPID.SetMode(AUTOMATIC);
+
 }
 
 void loop() {
-  // Serial.print("REG_ADC_MR = ");
-  // Serial.println(reg, HEX);
-  input = analogRead(A0);
-  // Serial.print(" input: ");
-  // Serial.println(input);
-  analogWrite(DAC1, input);
-  // delayMicroseconds(1);
-  // analogWrite(DAC1, 0);
-  // delayMicroseconds(1);
-  // i++;
-  // if (i == maxSamplesNum) i = 0; // Reset
+  Input = analogRead(A0);
+  myPID.Compute();
+  analogWrite(DAC1, Input);
 }
 //IDO 0544997948
