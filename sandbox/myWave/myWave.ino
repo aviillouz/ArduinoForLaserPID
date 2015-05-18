b9de00bd4cc910acd636ad630d8efb01acc5aefa
Avi <avi.illouz@gmail.com>
2015-05-18 13:52:42 +0300

    Cleaning

--- sandbox/myWave/myWave.ino
+++ sandbox/myWave/myWave.ino
@@ -1,37 +1,21 @@
 // myWave.ino
-// #include "Waveforms.h"
-#include <PID_v1.h>
+
 //Define Variables we'll be connecting to
 double Setpoint, Input, Output;
-
-//Specify the links and initial tuning parameters
-PID myPID(&Input, &Output, &Setpoint,2,5,0, DIRECT);
-
-int value = 0;
 int DACBits = 12;
-int input = 0;
-int gain = 1;
 
 void setup() {
-  Serial.begin(115200);
+  // Serial.begin(115200);
 	analogWriteResolution(DACBits);  
   analogReadResolution(DACBits);
   Input = analogRead(A0);
   Setpoint = 1000;
-
-  //turn the PID on
-  myPID.SetOutputLimits(0,4000);
-  myPID.SetMode(AUTOMATIC);
-
 }
 
 void loop() {
   Input = analogRead(A0);
-  myPID.Compute();
-  //find something simple to control on
-  //subl push
-  gain++;
   analogWrite(DAC1, gain*Input);
-  if (gain > 4) gain = 1;
 }
-//IDO 0544997948
+
+
+
