c6e7aa64d8e35abba3737beade3bcb381d92afac
Avi <avi.illouz@gmail.com>
2015-05-18 14:26:46 +0300

    PI Algorithm ported to arduino

--- sandbox/myWave/myWave.ino
+++ sandbox/myWave/myWave.ino
@@ -1,8 +1,13 @@
 // myWave.ino
 
-//Define Variables we'll be connecting to
-double Setpoint, Input, Output;
 int DACBits = 12;
+
+//PID global variables
+double Setpoint, Input, Output;
+int parameter k1 = 2;  
+int parameter k2 = 5;
+double previousSignal;
+double previousError;
 
 void setup() {
   // Serial.begin(115200);
@@ -13,9 +18,19 @@ void setup() {
 }
 
 void loop() {
+	//Read voltage from wavemeter
   Input = analogRead(A0);
+
+  //Convert to error from setpoint //TODO
+	double CurrentError;
+
+	//Calculate Output
+	Output = previousSignal + k1*CurrentError - k2*previousError;
+	previousError = CurrentError;
+	previousSignal = Output;
   analogWrite(DAC1, Output);
 }
+
 
 
 
